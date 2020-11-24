87dec597355771ed9982cf8fe0b01cef74c2be78
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2020-11-24 16:30:45 +0100

    working form fills and db calls

--- blueberry/routes.py
+++ blueberry/routes.py
@@ -1,11 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request, session, abort 
-from blueberry import app, db
-from blueberry import User
+from blueberry import app, db, User
 import socket
 
 @app.route("/login", methods=['POST'])
 def login():
-    if request.form['adminpass'] == 'blueberry':
+    account = User.query.filter_by(adminuser='admin').first()
+    if request.form['adminpass'] == account.adminpass:
         session['logged_in'] = True
     else:
         flash('Invalid Password')
@@ -22,9 +22,25 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
 
-    elif request.method == 'POST':
-        return render_template('admin.html', adminuser='admin')
-
     else:
         config = User.query.filter_by(adminuser='admin').first()
-        return render_template('admin.html', netname=config.netname)
+        return render_template('admin.html', netname=config.netname, nettype=config.nettype, netpass=config.netpass, adblock=config.adblock, adminuser=config, adminpass=config.adminpass)
+
+@app.route('/update', methods=["POST"])
+def update(): 
+        netname = request.form.get("netname")
+        nettype = request.form.get("nettype")
+        netpass = request.form.get("netpass")
+        adblock = request.form.get("adblock")
+        adminpass = request.form.get("adminpass")
+
+        update = User.query.filter_by(adminuser='admin').first()
+        update.netname = netname
+        update.nettype = nettype
+        update.netpass = netpass
+        update.adblock = adblock
+        update.adminpass = adminpass
+
+        db.session.commit()
+
+        return redirect("/")
